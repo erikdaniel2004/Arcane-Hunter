b7ce7bad1b37dd4cec9cd58c605ee7027e5c008d
erikdaniel2004 <erikdaniel.exposito@gmail.com>
2025-04-18 22:04:34 +0200

    Continuación del videojuego anteriormente inicializado Arcane Hunter, con enlace GitHub: https://github.com/erikdaniel2004/DANIELEXPOSITO_ALUMNO_ARCANEHUNTER.git. Los cambios que se han realizado desde entonces han sido la implementación de recursos con licencia comprada o gratuita para la realización del proyecto, lógica de ataque y daño a enemigos y correción de errores básicos

---
+++ juego-propio/characters/player/script/player_knight.gd
@@ -0,0 +1,179 @@
+extends CharacterBody2D
+
+@export var gravity_scale = 2
+@export var speed = 200
+@export var acceleration = 200
+@export var friction = 900
+@export var jump_force = -550
+@export var air_acceleration = 1500
+@export var air_friction = 700
+@export var max_health := 100
+
+
+@onready var ani_player = $ani_player
+@onready var time = $time
+@onready var timer_invincible = $timer_invincible
+@onready var audio_player = $audio_player
+@onready var attack_area = $attack_area
+@onready var bar_health = $bar_health/TextureProgressBar
+
+# Referencia al contador de monedas
+@onready var contador: Control = $CanvasLayer/contador_moneda
+
+# Referencia al contador de runas
+@onready var contador2: Control = $CanvasLayer2/contador_runa
+
+# Contadores
+var monedas = 0
+var runas = 0
+
+# Variable que determina si la gorgona ha sido herida
+var esta_herido = false
+
+# Variable con la que contabilizar la vida de la gorgona
+var current_health := max_health
+
+# Variable con la que hacer invulnerable al jugador cuando reciba un ataque
+var is_invulnerable := false
+
+# Altura límite para morir
+@export var death_y_threshold: float = 1500
+
+func _ready():
+	add_to_group("player_knight")
+	contador.actualizar(0)
+	contador2.actualizar(0)
+	bar_health.visible = false
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta * gravity_scale
+
+func handle_acceleration(input_axis, delta):
+	if not is_on_floor():
+		return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, speed * input_axis, acceleration * delta)
+
+func _physics_process(delta: float) -> void:
+	if esta_herido:
+		move_and_slide()
+		return
+
+	var input_axis = Input.get_axis("mover_izquierda", "mover_derecha")
+
+	# Si cae por debajo del límite, muere
+	if global_position.y > death_y_threshold:
+		morir()
+
+	apply_gravity(delta)
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	handle_jump()
+	handle_air_acceleration(input_axis, delta)
+	update_animation(input_axis)
+	move_and_slide()
+	if Input.is_action_just_pressed("atacar"):
+		atacar()
+	update_attack_area_direction()
+
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("saltar"):
+			velocity.y = jump_force
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor():
+		return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, speed * input_axis, air_acceleration * delta)
+
+func update_animation(input_axis):
+	if ani_player.is_playing() and ani_player.animation == "attack":
+		return
+	if input_axis != 0:
+		ani_player.speed_scale = velocity.length() / 100
+		ani_player.flip_h = (input_axis < 0)
+		ani_player.play("run")
+	elif not is_on_floor():
+		ani_player.play("jump")
+	else:
+		ani_player.speed_scale = 1
+		ani_player.play("idle")
+	
+# Función para sumar monedas
+func sumar_monedas(valor):
+	monedas += valor
+	contador.actualizar(monedas)
+	
+# Función para sumar runas
+func obtener_runa(valor):
+	runas += valor
+	contador2.actualizar(runas)
+
+# Función para la muerte del jugador
+func morir():
+	audio_player.play() 
+	ani_player.play("dead")
+	set_physics_process(false)
+	time.start()
+	await time.timeout
+	get_tree().reload_current_scene()
+	
+func recibir_dano(cantidad):
+	if is_invulnerable:
+		return
+
+	current_health -= cantidad
+	current_health = clamp(current_health, 0, max_health)
+	bar_health.value = current_health
+	bar_health.visible = true
+	$timer_bar.start()
+
+	esta_herido = true
+	set_physics_process(false)
+	ani_player.play("hurt")
+	await ani_player.animation_finished
+	esta_herido = false
+	set_physics_process(true)
+
+	if current_health <= 0:
+		morir()
+	else:
+		hacer_invulnerable_temporalmente()
+
+func hacer_invulnerable_temporalmente():
+	is_invulnerable = true
+	$timer_invincible.start()  # Asegúrate de tener un Timer como hijo del jugador
+
+func _on_timer_invincible_timeout():
+	is_invulnerable = false
+
+func atacar():
+	ani_player.play("attack")
+	var area = attack_area.get_overlapping_bodies()
+
+	for cuerpo in area:
+		if cuerpo.is_in_group("enemigos"):
+			if cuerpo.has_method("recibir_dano"):
+				cuerpo.recibir_dano(25)
+
+func recuperar_vida(cantidad):
+	current_health += cantidad
+	current_health = clamp(current_health, 0, max_health)
+	bar_health.value = current_health
+
+func update_attack_area_direction():
+	var offset = 20
+	if ani_player.flip_h:
+		attack_area.position.x = -offset
+	else:
+		attack_area.position.x = offset
+
+func _on_timer_bar_timeout():
+	bar_health.visible = false
